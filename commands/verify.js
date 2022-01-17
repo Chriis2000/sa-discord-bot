e5ca0fb37b068567d90bd029e6d362080b922d0a
Lewis <96670124+streeamm@users.noreply.github.com>
2022-01-17 01:05:50 +0000

    Updating Verify

--- commands/verify.js
+++ commands/verify.js
@@ -2,13 +2,6 @@ const verify = async (reaction, user, a) => {
   const verifiedRole = reaction.message.guild.roles.cache.get('921878464276070450');
   const member = reaction.message.guild.members.cache.get(user.id);
 
-  if (a === false) {
-    if (member.roles.cache.some(role => role.id === '921878464276070450')) {
-      member.roles.remove('921878464276070450');
-    };
-    return console.log(`Unverified: ${user.username}#${user.discriminator}`);
-  };
-
   if (reaction.partial) {
 		try {
 			await reaction.fetch();
@@ -18,8 +11,15 @@ const verify = async (reaction, user, a) => {
 		}
 	};
 
-  if (reaction._emoji.name !== "🍌") return;
   if (reaction.message.id !== '924418250727641099') return;
+  if (reaction._emoji.name !== "🍌") return;
+
+  if (a === false) {
+    if (member.roles.cache.some(role => role.id === '921878464276070450')) {
+      member.roles.remove('921878464276070450');
+    };
+    return console.log(`Unverified: ${user.username}#${user.discriminator}`);
+  };
 
   member.roles.add(verifiedRole);
   
